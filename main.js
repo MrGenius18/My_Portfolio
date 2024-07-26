390fdc92b66f9231875c89995f5f8bdf5682dcd0
Bhautik Gondaliya <mr.genius0180@gmail.com>
2024-07-26 10:58:59 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text", {
+    strings: ["Data Scientist" , "Python Developer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    backDelay: 800,
+    loop: true
+});
